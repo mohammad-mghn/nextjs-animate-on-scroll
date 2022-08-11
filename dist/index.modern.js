cc58b9fa2bbdea90a0af41f015df8a8e38df971c
vito mohagheghianm <vito.mohagheghian@gmail.com>
2022-08-11 15:40:05 +0430

    release new version #4

--- dist/index.modern.js
+++ dist/index.modern.js
@@ -2,31 +2,50 @@ import React from 'react';
 import { motion } from 'framer-motion/dist/framer-motion';
 export { AnimatePresence as AnimateProvider } from 'framer-motion/dist/framer-motion';
 
-const Animator = props => {
-  const {
-    children,
-    UNKey = 1,
-    bounce = 0.5,
-    amountDisplay = 0.25,
-    initial = {
-      scale: 1.2,
-      opacity: 0
-    },
-    onScreen = {
-      scale: 1,
-      opacity: 1
+function _extends() {
+  _extends = Object.assign ? Object.assign.bind() : function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
     }
-  } = props;
-  const Varients = {
-    offscreen: { ...initial
-    },
-    onscreen: { ...onScreen,
+
+    return target;
+  };
+  return _extends.apply(this, arguments);
+}
+
+var Animator = function Animator(props) {
+  var children = props.children,
+      _props$UNKey = props.UNKey,
+      UNKey = _props$UNKey === void 0 ? 1 : _props$UNKey,
+      _props$bounce = props.bounce,
+      bounce = _props$bounce === void 0 ? 0.5 : _props$bounce,
+      _props$amountDisplay = props.amountDisplay,
+      amountDisplay = _props$amountDisplay === void 0 ? 0.25 : _props$amountDisplay,
+      _props$initial = props.initial,
+      initial = _props$initial === void 0 ? {
+    scale: 1.2,
+    opacity: 0
+  } : _props$initial,
+      _props$onScreen = props.onScreen,
+      onScreen = _props$onScreen === void 0 ? {
+    scale: 1,
+    opacity: 1
+  } : _props$onScreen;
+  var Varients = {
+    offscreen: _extends({}, initial),
+    onscreen: _extends({}, onScreen, {
       transition: {
         type: 'spring',
         bounce: bounce,
         DelayNode: 1
       }
-    }
+    })
   };
   return /*#__PURE__*/React.createElement(motion.div, {
     key: UNKey,
